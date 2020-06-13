50c1867667c7a6c27a5472c72a03affbe988adf0
hawk-praxs <pratyush.11708251@lpu.in>
2020-06-13 13:13:20 +0530

    Completed SVM and Decision Tree Lessons
    SVM answers vary for the GitHub Intro to ML Course.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,13 +19,11 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+print('Accuracy:',accuracy_score(labels_test,pred))
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,12 +19,32 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+clf = svm.SVC(kernel='rbf', C=10000)
 
+# Slicing to 1% of the dataset for low training time but reduced accuracy
+#features_train = features_train[:int(len(features_train)//100)]
+#labels_train = labels_train[:int(len(labels_train)//100)]
 
-#########################################################
-### your code goes here ###
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training time:", round(time()-t0, 3), "s")
 
-#########################################################
+t1 = time()
+pred = clf.predict(features_test) # Zero-indexed list
+print("Prediction time:", round(time()-t1, 3), "s")
 
+print('Score: ', accuracy_score(labels_test, pred))
 
+def check_Chris(pred):
+    n = 0
+
+    for p in pred:
+        if p == 1:
+            n+=1
+
+    return n
+
+print('Chris prediction no.: ', check_Chris(pred))
