bce835acb2d0340144d19695218471f7228ee4c6
hawk-praxs <pratyush.11708251@lpu.in>
2020-06-14 12:16:41 +0530

    Completed Lesson 5 Ex
    Used KNN for gaining highest accuracy of 94.4%

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -28,14 +28,27 @@ plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 
+clf = AdaBoostClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print('Accuracy: ', accuracy_score(labels_test, pred))
 
+clf = RandomForestClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print('Accuracy: ', accuracy_score(labels_test, pred))
 
-
-
+clf = KNeighborsClassifier(n_neighbors=22)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print('Accuracy: ', accuracy_score(labels_test, pred))
 
 
 try:
