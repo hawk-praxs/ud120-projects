9b6ab24813ee1dae4fee22803f0daee3a28a9bc4
hawk-praxs <pratyush.11708251@lpu.in>
2020-06-17 12:58:36 +0530

    Completed Lesson 8: Outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,17 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
-    ### your code goes here
+    error = [abs(net_worths[i]-predictions[i]) for i in range(len(net_worths))]
 
-    
+    for i in range(len(net_worths)):
+        if error[i] <= 80:
+            cleaned_data.append((ages[i], net_worths[i], error[i]))
+
     return cleaned_data
 
