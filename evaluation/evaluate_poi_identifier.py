c433c9dd39e67a072ec4d0c1bc1d4325d7c8601a
hawk-praxs <pratyush.11708251@lpu.in>
2020-06-23 15:01:07 +0530

    Completed Lesson 15

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -16,7 +16,7 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
@@ -26,6 +26,40 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### your code goes here 
+### your code goes here
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, recall_score, precision_score
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+total = len([e for e in labels_test if e == 1.0])
+
+print('POIs in test set: ', total)
+print('Predicted values length: ', len(pred))
+print('Accuracy: ', accuracy_score(labels_test, pred))
+print('Accuracy of biased identifier: ', float(1-(total/len(pred))))
+print('Precision of biased identifier: ', precision_score(labels_test, pred))
+print('Recall of biased identifier: ', recall_score(labels_test, pred))
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+true_positives = [i for (i,j) in zip(predictions, true_labels) if i == j]
+print('True Positives in given Values: ', sum(true_positives))
+
+true_negatives = len(true_positives) - sum(true_positives)
+print('True Negatives in given Values: ', true_negatives)
+
+false_positives = [i for (i,j) in zip(predictions, true_labels) if i != j]
+print('False Positives in given values: ', sum(false_positives))
+
+false_negatives = len(false_positives) - sum(false_positives)
+print('False Negatives in given values: ', false_negatives)
+
+print('Precision of given values: ', precision_score(true_labels, predictions))
+print('Recall of given values: ', recall_score(true_labels, predictions))
