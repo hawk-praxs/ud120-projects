dafd62c315e9b4efc3d31ae20a72e0cfb4f89a25
hawk-praxs <pratyush.11708251@lpu.in>
2020-06-23 13:25:02 +0530

    Completed Lesson 14
    Incorrect accuracy after splitting dataset as 0.68 instead of 0.72

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -15,7 +15,7 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -27,6 +27,21 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### it's all yours from here forward!  
+### it's all yours from here forward!
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
+clf = DecisionTreeClassifier()
 
+clf.fit(features,labels)
+pred = clf.predict(features)
+
+print('Accuracy before splitting: ', accuracy_score(labels, pred))
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+print('Accuracy after splitting: ', accuracy_score(labels_test, pred))
